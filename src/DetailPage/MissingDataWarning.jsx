11a40c4df0d14f0b0b75c3667a4d23aea31948d7
macaul_b <budmacaulay97@outlook.com>
2025-08-11 10:43:38 +0200

    failed fetch error page

---
+++ src/DetailPage/MissingDataWarning.jsx
@@ -0,0 +1,44 @@
+import { Container, Button } from "react-bootstrap";
+import TitleAndLogo from "../common/TitleAndLogo";
+import MaterialsCloudHeader from "mc-react-header";
+
+// full page error component to handle when data is missing.
+export default function MissingDataWarning({ params, navigate }) {
+  return (
+    <MaterialsCloudHeader
+      activeSection="discover"
+      breadcrumbsPath={[
+        { name: "Discover", link: "https://www.materialscloud.org/discover" },
+        {
+          name: "Materials Cloud Three-Dimensional Structure Database",
+          link: `${import.meta.env.BASE_URL}`,
+        },
+        { name: `${params.id}/${params.method}`, link: null },
+      ]}
+    >
+      <Container fluid="xxl">
+        <TitleAndLogo />
+        <div
+          style={{
+            textAlign: "center",
+            marginTop: "5rem",
+            marginBottom: "10rem",
+          }}
+        >
+          <div
+            style={{
+              color: "red",
+              fontSize: "1.2rem",
+              marginBottom: "2rem",
+            }}
+          >
+            {`Warning: Fetching of data failed, suggesting that ${params.id} has not been calculated with ${params.method}`}
+          </div>
+          <Button variant="primary" onClick={() => navigate("/")}>
+            Return to MC3D homepage
+          </Button>
+        </div>
+      </Container>
+    </MaterialsCloudHeader>
+  );
+}
--- src/DetailPage/index.jsx
+++ src/DetailPage/index.jsx
@@ -12,7 +12,7 @@ import TitleAndLogo from "../common/TitleAndLogo";
 
 import { formatTitle } from "../common/utils";
 
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 import {
   loadMetadata,
@@ -27,24 +27,42 @@ import ProvenanceSection from "./ProvenanceSection";
 import XrdSection from "./XrdSection";
 import SuperConductivity from "./Superconductivity";
 
+// if fetching fails we use this.
+import MissingDataWarning from "./MissingDataWarning";
+
 // contributed sections
 
 // import RelatedSection from "./RelatedSection";
 
 async function fetchCompoundData(method, id) {
-  let metadata = await loadMetadata(method, id);
-  let details = await loadDetails(method, id);
+  try {
+    const [metadata, details] = await Promise.all([
+      loadMetadata(method, id),
+      loadDetails(method, id),
+    ]);
 
-  let structureUuid = details.general.structure_uuid;
+    const structureUuid = details?.general?.structure_uuid;
+    if (!structureUuid) throw new Error("Missing structure UUID");
 
-  let aiidaAttributes = await loadAiidaAttributes(method, structureUuid);
-  let structureCif = await loadAiidaCif(method, structureUuid);
+    const [aiidaAttributes, structureCif] = await Promise.all([
+      loadAiidaAttributes(method, structureUuid),
+      loadAiidaCif(method, structureUuid),
+    ]);
 
-  return {
-    metadata: metadata,
-    details: details,
-    structureInfo: { aiidaAttributes: aiidaAttributes, cif: structureCif },
-  };
+    return {
+      metadata,
+      details,
+      structureInfo: { aiidaAttributes, cif: structureCif },
+      missingStructureWarning: null,
+    };
+  } catch {
+    return {
+      metadata: null,
+      details: null,
+      structureInfo: { aiidaAttributes: null, cif: null },
+      missingStructureWarning: true,
+    };
+  }
 }
 
 function DetailPage() {
@@ -59,19 +77,44 @@ function DetailPage() {
       console.log("Loaded general data", loadedData);
       setLoadedData(loadedData);
     });
-  }, [params.id, params.method]); // <- call when route params change
+  }, [params.id, params.method]);
 
-  let breadcrumbTitle = `${params.id}/${params.method}`;
-  let title = null;
-
-  let loading = loadedData == null;
-  if (!loading) {
-    title = formatTitle(
-      loadedData.details.general.formula,
-      params.id,
-      params.method,
+  // While loading, show spinner
+  if (loadedData === null) {
+    return (
+      <MaterialsCloudHeader
+        activeSection={"discover"}
+        breadcrumbsPath={[
+          { name: "Discover", link: "https://www.materialscloud.org/discover" },
+          {
+            name: "Materials Cloud Three-Dimensional Structure Database",
+            link: `${import.meta.env.BASE_URL}`,
+          },
+          { name: `${params.id}/${params.method}`, link: null },
+        ]}
+      >
+        <Container fluid="xxl">
+          <TitleAndLogo />
+          <div style={{ width: "150px", padding: "40px", margin: "0 auto" }}>
+            <McloudSpinner />
+          </div>
+        </Container>
+      </MaterialsCloudHeader>
     );
   }
+
+  // if Data is missing we show the Error.
+  if (loadedData?.missingStructureWarning) {
+    return <MissingDataWarning params={params} navigate={navigate} />;
+  }
+
+  // Otherwise proceed as normal.
+  const title = formatTitle(
+    loadedData.details.general.formula,
+    params.id,
+    params.method,
+  );
+
   return (
     <MaterialsCloudHeader
       activeSection={"discover"}
@@ -81,26 +124,18 @@ function DetailPage() {
           name: "Materials Cloud Three-Dimensional Structure Database",
           link: `${import.meta.env.BASE_URL}`,
         },
-        { name: breadcrumbTitle, link: null },
+        { name: `${params.id}/${params.method}`, link: null },
       ]}
     >
       <Container fluid="xxl">
         <TitleAndLogo />
-        {loading ? (
-          <div style={{ width: "150px", padding: "40px", margin: "0 auto" }}>
-            <McloudSpinner />
-          </div>
-        ) : (
-          <>
-            <div className="detail-page-heading">{title}</div>
-            <OverviewSection params={params} loadedData={loadedData} />
-            <StructureSection params={params} loadedData={loadedData} />
-            <ProvenanceSection params={params} loadedData={loadedData} />
-            <XrdSection params={params} loadedData={loadedData} />
-            <SuperConductivity params={params} loadedData={loadedData} />
-            {/* <RelatedSection /> */}
-          </>
-        )}
+        <div className="detail-page-heading">{title}</div>
+        <OverviewSection params={params} loadedData={loadedData} />
+        <StructureSection params={params} loadedData={loadedData} />
+        <ProvenanceSection params={params} loadedData={loadedData} />
+        <XrdSection params={params} loadedData={loadedData} />
+        <SuperConductivity params={params} loadedData={loadedData} />
+        {/* <RelatedSection /> */}
       </Container>
     </MaterialsCloudHeader>
   );
