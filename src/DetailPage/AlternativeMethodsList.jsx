d63d376795f17b1dc34eda7996b568ea0b227228
macaul_b <budmacaulay97@outlook.com>
2025-11-26 13:39:34 +0100

    links instead of buttons

--- src/DetailPage/AlternativeMethodsList.jsx
+++ src/DetailPage/AlternativeMethodsList.jsx
@@ -1,23 +1,15 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-
 const order = ["pbesol-v2", "pbesol-v1", "pbe-v1"];
 
-// simple component that takes metods and currentMethods and returns clickable buttons and some text string
+// simple component that takes methods and currentMethods and returns clickable buttons and some text string
 export default function MethodologyButtons({
   commentString = "This structure has been relaxed with the following methodologies:",
   id,
   methods,
   currentMethod,
 }) {
-  const navigate = useNavigate();
-
   if (!methods || Object.keys(methods).length === 0) return null;
 
-  // Define preferred ordering
-
-  // Sort keys according to the order array
-  const keys = Object.keys(methods).sort((a, b) => {
+  const sortedKeys = Object.keys(methods).sort((a, b) => {
     const ia = order.indexOf(a);
     const ib = order.indexOf(b);
     if (ia === -1 && ib === -1) return a.localeCompare(b);
@@ -25,28 +17,28 @@ export default function MethodologyButtons({
     if (ib === -1) return -1;
     return ia - ib;
   });
-  methods;
-
-  const handleClick = (key) => {
-    if (key === currentMethod) return;
-    navigate(`/details/${id}/${key}`);
-  };
 
   return (
     <div className="my-1">
-      <p className=" d-inline-block me-2">{commentString}</p>
+      <p className="d-inline-block me-2">{commentString}</p>
+
       <div className="d-inline-flex flex-wrap gap-1 align-items-center">
-        {keys.map((key) => (
-          <button
-            key={key}
-            onClick={() => handleClick(key)}
-            className={`btn btn-sm rounded-lg px-1 ${
-              key === currentMethod ? "btn-secondary disabled" : "btn-primary"
-            }`}
-          >
-            {key}
-          </button>
-        ))}
+        {sortedKeys.map((key) => {
+          const url = `#/details/${id}/${key}`;
+
+          return (
+            <a
+              key={key}
+              href={url}
+              rel="noopener noreferrer"
+              className={`btn btn-sm rounded-lg px-1 ${
+                key === currentMethod ? "btn-secondary disabled" : "btn-primary"
+              }`}
+            >
+              {key}
+            </a>
+          );
+        })}
       </div>
     </div>
   );
